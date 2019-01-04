9d33c9c407e1691a1c8ca1101b079bf28f907a96
Bailey Harrison <baileyharrison04@outlook.com>
2019-01-04 13:08:53 +0000

    Revert "Revert "testsync""
    This reverts commit f00c62809c76164e9deedcb2789e7c54143ab14d.

--- remoterun.sh
+++ remoterun.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./upload.sh
+ssh user@calculator "cd picalc/ && git pull"
 ssh user@calculator "echo 'started program' && sudo python3 -u picalc/main.py"
--- upload.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "please wait..."
-rsync -r --delete-before . user@calculator:~/picalc/
