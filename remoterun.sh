0335c36f5d5e777741b972754b171e770b24c40e
Bailey Harrison <baileyharrison04@outlook.com>
2019-01-04 13:52:08 +0000

    fixes

--- remoterun.sh
+++ remoterun.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ssh user@calculator "cd picalc && git pull"
-ssh user@calculator "echo 'started program' && sudo python3 -u picalc/main.py"
+ssh user@calculator "cd picalc/ && git pull"
+ssh user@calculator "sudo python3 -u picalc/main.py"
